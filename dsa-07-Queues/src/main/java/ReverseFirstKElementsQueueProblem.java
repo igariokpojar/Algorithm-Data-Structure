1060f227b2c6cd571928bfa2289e1f5df0d5e2ae
Igariok <igariokpojar@yahoo.com>
2022-10-30 10:34:15 -0500

    ReverseFirstKElementsQueueProblem

--- dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
+++ dsa-07-Queues/src/main/java/ReverseFirstKElementsQueueProblem.java
@@ -29,3 +29,10 @@ public class ReverseFirstKElementsQueueProblem {
         }
         return queue;    }
 }
+/*
+1. Given an integer K and a queue of integers, write code to reverse the order
+of the first K elements of the queue.
+-Input: Q = [10, 20, 30, 40, 50], K = 4
+-Output: Q = [40,30, 20, 10,50]
+Hint: You can use stack to reverse the order of first K elements
+ */
