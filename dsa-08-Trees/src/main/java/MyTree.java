16908617598a900aa3e8d3541232eb9fccf0d041
Igariok <igariokpojar@yahoo.com>
2022-10-30 13:52:16 -0500

    In order Traversal

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -1,33 +1,53 @@
 public class MyTree {
-
     TNode root;
 
-    public MyTree(TNode root) {
-        this.root = root;
+    public MyTree() {
     }
-    void insert(int value) {
-        TNode newNode = new TNode(value);
-        if (root == null) {
-            root = newNode;
+    void insert(int value){
+        TNode newNode=new TNode(value);
+        if (root==null) {
+            root=newNode;
             return;
         }
-        TNode current = root;
-        while (true) {
-            if (value <= current.value) {
-                if (current.leftChild == null) {
+        TNode current =root;
+        while(true){
+            if (value<= current.value){
+
+                if (current.leftChild==null){
                     // if left is null insert there!!!
-                    current.leftChild = newNode;
+                    current.leftChild=newNode;
                     break;
                 }
-                current = current.leftChild;
-            } else {
-                if (current.rightChild == null) {
+                //if leftChild is not null branch into left subtree!!
+                current=current.leftChild;
+            }
+            else {
+                if (current.rightChild==null){
                     // if right is null insert there!!!
-                    current.rightChild = newNode;
+                    current.rightChild=newNode;
                     break;
                 }
-                current = current.rightChild;
+                //if rightChild is not null branch into right subtree!!
+                current=current.rightChild;
             }
         }
     }
+
+    //PreOrder traversal of the tree
+    // Root-Left-right
+    void preOrderTraversal(TNode root){
+if (root==null)return;
+        System.out.print(root.value + ", ");
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+    }
+
+    void inOrderTraversal(TNode root){
+        if (root==null)return;
+
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value + ", ");
+        inOrderTraversal(root.rightChild);
+    }
+
 }
