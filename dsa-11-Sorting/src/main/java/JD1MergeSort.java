409b52b56e749496b245989a9d9236fbd60d767a
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2022-12-22 19:02:38 -0600

    MergeSort

--- dsa-11-Sorting/src/main/java/JD1MergeSort.java
+++ dsa-11-Sorting/src/main/java/JD1MergeSort.java
@@ -8,13 +8,20 @@ public class JD1MergeSort {
        for (int i = mid; i < array.length ; i++) rightArray[i-mid]=array[i];
        mergeSort(leftArray);
        mergeSort(rightArray);
+       //And then I'm going to combine these left and right solutions.
        merge(leftArray, rightArray, array);
 
 
    }
     public static void merge(int[] left, int[] right, int[] result){
+       //So I need 3 pointers,one for the left array, one for the right array one for the result.
+        // Because I'm going to combine these 2 arrays and come up with the result,and I need one pointer for each.
         int i=0,j=0,k=0;
-       while(i<left.length && j<right.length){
+        //So what I will do is first. I will start to compare the left array and the write-ring
+        //And if the element on the left array is less than the element on the right array.
+        // I'm going to pick up the element from the left array, and I'm going to increase the pointer on the left array
+       while(i<left.length && j<right.length){// this while loop is for checking. If I am at one of the end of the list.
+
            if (left[i]<=right[j]) result[k++]=left[i++];
            else result[k++]=right[j++];
        }
